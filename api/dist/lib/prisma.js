b3e6e5081ac3feb9539e62ffd79824fe48f52531
Shivam Bansal <shivamsb2003@gmail.com>
2024-07-09 20:48:22 +0530

    fixed ts error

--- api/dist/lib/prisma.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
-/* Inside src folder */
-("npx prisma db push");
-("npx prisma generate");
